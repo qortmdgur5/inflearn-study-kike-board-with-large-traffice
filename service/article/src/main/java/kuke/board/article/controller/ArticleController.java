a0f782d8ad11e969fac062d74af76d4e355b82f0
bsh <dnfg12@naver.com>
2025-04-16 18:46:13 +0900

    Article 데이터 Page 네이션을 위한 테스트 및 코드 작성

--- service/article/src/main/java/kuke/board/article/controller/ArticleController.java
+++ service/article/src/main/java/kuke/board/article/controller/ArticleController.java
@@ -3,6 +3,7 @@ package kuke.board.article.controller;
 import kuke.board.article.service.ArticleService;
 import kuke.board.article.service.request.ArticleCreateRequest;
 import kuke.board.article.service.request.ArticleUpdateRequest;
+import kuke.board.article.service.response.ArticlePageResponse;
 import kuke.board.article.service.response.ArticleResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -12,21 +13,34 @@ import org.springframework.web.bind.annotation.*;
 public class ArticleController {
     private final ArticleService articleService;
 
+    // articleId 에 해당 하는 Article 조회
     @GetMapping("/v1/articles/{articleId}")
     public ArticleResponse read(@PathVariable(name = "articleId") Long articleId){
         return articleService.read(articleId);
     }
 
+    @GetMapping("/v1/articles")
+    public ArticlePageResponse readAll(
+            @RequestParam("boardId") Long boardId,
+            @RequestParam("page") Long page,
+            @RequestParam("pageSize") Long pageSize
+    ) {
+        return articleService.readAll(boardId, page, pageSize);
+    }
+
+    // Article 전체 조회
     @PostMapping("/v1/articles")
     public ArticleResponse create(@RequestBody ArticleCreateRequest request){
         return articleService.create(request);
     }
 
+    // articleId 에 해당 하는 정보 수정
     @PutMapping("/v1/articles/{articleId}")
     public ArticleResponse update(@PathVariable(name = "articleId") Long articleId,@RequestBody ArticleUpdateRequest request){
         return articleService.update(articleId, request);
     }
 
+    // articleId 삭제
     @DeleteMapping("/v1/articles/{articleId}")
     public void delete(@PathVariable(name = "articleId") Long articleId){
         articleService.delete(articleId);
