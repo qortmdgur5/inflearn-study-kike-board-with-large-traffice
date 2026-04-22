0f611c43c8b392ca9df4ea804731a723ad266f61
bsh <dnfg12@naver.com>
2026-04-22 19:47:40 +0900

    메세지 릴레이 코디네이터 구성 - 카프카 메세지 발행 및 대기 메세지 후처리

--- common/outbox-message-relay/src/main/java/kuke/board/common/outboxmessagerelay/MessageRelayConfig.java
+++ common/outbox-message-relay/src/main/java/kuke/board/common/outboxmessagerelay/MessageRelayConfig.java
@@ -3,9 +3,9 @@ package kuke.board.common.outboxmessagerelay;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -17,7 +17,7 @@ import java.util.Map;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
-@Configuration
+@AutoConfiguration
 @EnableAsync
 @ComponentScan("kuke.board.common.outboxmessagerelay")
 @EnableScheduling
