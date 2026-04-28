0283911bd854e9e2f764801ced37f826ad26d1f0
bsh <dnfg12@naver.com>
2026-04-28 18:08:03 +0900

    Article Query 읽기 서비스 전체 이벤트 핸들러 구현 (handle, supports)

---
+++ service/article-read/src/main/java/kuke/board/articleread/service/event/handler/ArticleDeletedEventHandler.java
@@ -0,0 +1,25 @@
+package kuke.board.articleread.service.event.handler;
+
+import kuke.board.articleread.repository.ArticleQueryModelRepository;
+import kuke.board.common.event.Event;
+import kuke.board.common.event.EventType;
+import kuke.board.common.event.payload.ArticleDeletedEventPayload;
+import kuke.board.common.event.payload.ArticleUpdatedEventPayload;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ArticleDeletedEventHandler implements EventHandler<ArticleDeletedEventPayload>{
+    private final ArticleQueryModelRepository articleQueryModelRepository;
+
+    @Override
+    public void handle(Event<ArticleDeletedEventPayload> event) {
+        articleQueryModelRepository.delete(event.getPayload().getArticleId());
+    }
+
+    @Override
+    public boolean supports(Event<ArticleDeletedEventPayload> event) {
+        return EventType.ARTICLE_DELETED.equals(event.getType());
+    }
+}
