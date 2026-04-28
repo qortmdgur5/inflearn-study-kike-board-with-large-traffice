855fd8f901bd83bc41ab782b890ab3efe1c3386f
bsh <dnfg12@naver.com>
2026-04-28 18:24:19 +0900

    읽기 서비스 조회수 캐싱전략 적용 - 1초

--- service/article-read/src/main/java/kuke/board/articleread/client/ViewClient.java
+++ service/article-read/src/main/java/kuke/board/articleread/client/ViewClient.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -21,7 +22,12 @@ public class ViewClient {
         restClient = RestClient.create(viewServiceUrl);
     }
 
+    // article-read 서비스(현재 코드 서비스) 의 redis 에서 캐싱된 데이터를 조회한다,
+    // 없으면 view count 서비스 클라이언트를 호출해서 viewCount API 를 호출한다.
+    // 이후 로직은 해당 서비스에서 할테니 우리는 몰라도 된다. 이게 설계의 원칙.
+    @Cacheable(key = "#articleId", value = "articleViewCount")
     public long count(Long articleId) {
+        log.info("[ViewCline.count] articleId = {}",  articleId);
         try {
             return restClient.get()
                     .uri("/v1/article-views/articles/{articleId}/count", articleId)
